98be56b8f8b0b14d64df0e597f73abf156849f42
Kyler Ramsey <104465783+kylerramsey@users.noreply.github.com>
2022-08-01 20:26:02 -0400

    changing styling

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,22 +7,35 @@ export default function Login() {
     return (
         <>
             <div className="app">
-                <div className="container">
-                    <ul className="login">
-                        {user.loggedIn ? (
-                            <li>
-                                <button onClick={logout}>Logout</button>
-                            </li>
-                        ) : (
-                            <li>
-                                <button onClick={login}>
-                                    Click Here to Sign in with Google
-                                </button>
-                            </li>
-                        )}
-                    </ul>
+                <div className="container mt-30 text-center">
+                    <section>
+                        <h2 className="login">Welcome to your Weather App</h2>
+                        <p className="login">
+                            Click below to sign in with Google
+                            <br />
+                            <span className="line">
+                                <a onClick={login} href="#">
+                                    Sign In
+                                </a>
+                            </span>
+                        </p>
+                    </section>
                 </div>
             </div>
         </>
     );
+}
+
+{
+    /* <ul className="login">
+    {user.loggedIn ? (
+        <li>
+            <button onClick={logout}>Logout</button>
+        </li>
+    ) : (
+        <li>
+            <button onClick={login}>Click Here to Sign in with Google</button>
+        </li>
+    )}
+</ul>; */
 }
